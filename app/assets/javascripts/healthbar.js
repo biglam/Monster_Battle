d71d5bb2b0bb31fab1e35256776040fd7c21a429
Craig Lamont <craig@techiteasy.org.uk>
2015-12-08 13:23:19 +0000

    refactor and fix health bars

---
+++ app/assets/javascripts/healthbar.js
@@ -0,0 +1,16 @@
+$(function(){ 
+  //jQuery code here 
+
+});
+
+var healthbar_update = function() {
+  $('#health1').val(m1health0);
+  $('#health2').val(m1health1);
+  $('#health3').val(m1health2);
+  $('#health4').val(m1health3);
+
+  $('#health5').val(m2health0);
+  $('#health6').val(m2health1);
+  $('#health7').val(m2health2);
+  $('#health8').val(m2health3);
+};
