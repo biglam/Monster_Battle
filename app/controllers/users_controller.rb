163d5187457390af4d0bd25619d6e911472d40f1
graemekean <graemekean@gmail.com>
2015-12-09 14:11:16 +0000

    Added gender to user model and created user update page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    @user.update(params[:user].permit(:name,:email, :gender))
+    
+    # year_of_birth = params[:user][:date_of_birth].to_date.year
+    
+    # @user.update(user_params) #from the private method below - whitelist check
+
+    redirect_to(user_path)
+
+   end
+
+   # private
+
+   #  def user_params
+   #   params.require(:user).permit(:name, :email, :password,
+   #    :password_confirmation, :id, :gender)
+   # end
+
 end
