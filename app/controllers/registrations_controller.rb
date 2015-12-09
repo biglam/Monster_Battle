f51744d494ecd20c3012f67f5b737f2e4e496a52
Craig Lamont <craig@techiteasy.org.uk>
2015-12-09 23:50:39 +0000

    avatars implemented in battle

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,10 +7,10 @@ class RegistrationsController < Devise::RegistrationsController
   # end
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar, :name)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :avatar, :name)
   end
 end
